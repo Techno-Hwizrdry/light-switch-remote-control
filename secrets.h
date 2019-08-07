268921f86873ac97586a1a2268251fd6abb921c1
Techno-Hwizrdry <alex.mardigian@gmail.com>
2019-08-07 17:33:53 -0400

    Added the source code.

---
+++ secrets.h
@@ -0,0 +1,11 @@
+/*
+ * Enter your wifi network and MQTT server settings in this file.
+ */
+
+const char* NETWORK_SSID = "";
+const char* NETWORK_PASSWORD = "";
+
+const char* MQTT_SERVER = "";
+const char* MQTT_USER = "";
+const char* MQTT_PASSWORD = "";
+const char* MQTT_TOPIC = "";
